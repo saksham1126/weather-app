2c688f79d600f6a84af509c78ce5f03a7d8d41c9
saksham1126 <sakshampathak100@gmail.com>
2025-10-22 22:26:18 +0530

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": '0cd3c98d87msh1952baf9ebcce40p10b50ejsn6f3a63709196',
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = 'b5b776d5dc5f6841c13fe17a50c5f672'; 
